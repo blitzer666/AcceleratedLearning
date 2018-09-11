0e65c7972c0aad560b898b5c33a87e4058eb9b7f
AWA5CG8126BTQ\Administrator <petergo2school@gmail.com>
2018-09-11 21:51:26 +0200

    Migrerar första gången efter att fått Context att koppla till Samurai i domain....

--- EF/EfSamurai.Data/EfSamurai.Data/SamuraiContext.cs
+++ EF/EfSamurai.Data/EfSamurai.Data/SamuraiContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using EfSamurai.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace EfSamurai.Domain
+
+namespace EfSamurai.Data
 {
-    class SamuraiContext
+    public class SamuraiContext : DbContext
     {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai;");
+        }
     }
 }
--- EF/EfSamurai.Data/EfSamurai.Domain/Samurai.cs
+++ EF/EfSamurai.Data/EfSamurai.Domain/Samurai.cs
@@ -8,6 +8,6 @@ namespace EfSamurai.Domain
 {
     public class Samurai
     {
-        
+        public int Id { get; set; }
     }
 }
