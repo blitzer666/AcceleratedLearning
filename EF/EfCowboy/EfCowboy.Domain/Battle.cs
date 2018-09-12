0fd0840227fa05ed500eafe1e0894528356659c2
AWA5CG8126BTQ\Administrator <petergo2school@gmail.com>
2018-09-12 11:40:58 +0200

    Lagt till Battle-Klass

--- EF/EfCowboy/EfCowboy.Domain/Battle.cs
+++ EF/EfCowboy/EfCowboy.Domain/Battle.cs
@@ -8,6 +8,11 @@ namespace EfCowboy.Domain
 {
     public class Battle
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Brutal { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
     }
 }
